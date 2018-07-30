d368ab119dca7bec891f390eeef1357cf558254e
Dylan Belvedere <tinman.dylan@gmail.com>
2018-07-29 19:34:49 -0700

    Added a basic form to get information from member, the form ensures all fields have been filled. When submit is pressed it is sent to a handler which currently just redirects to the waiver page, in the future here can have the form values to a google spreadsheet and also store for use on the waiver.

--- client/src/Signin.js
+++ client/src/Signin.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 import logo from './logo.svg';
+/* maybe all this stuff needs to be server side
+var GoogleSpreadsheet = require('google-spreadsheet');
+
+var doc = new GoogleSpreadsheet('1EnS4hlkup-CNL8jCn3oWhtHBzMud3pbbUfu_q0kXI9w');*/
+
+var isEnabled = false;
 
 
   function NewLine(idVal){
@@ -19,12 +25,18 @@ class Signin extends Component {
     super(props);
     this.state = {
       users: [],
+      firstInput: '',
+      lastInput: '',
+      emailInput: '',
+      phoneInput: '',
     };
     axios
     .get('/buttons') 
       .then(res=>{this.setState({ users: res.data })});
    
     this.handleButton = this.handleButton.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
   
 }
 
@@ -38,13 +50,57 @@ class Signin extends Component {
     console.log(response);
     })}
 
+    handleChange(event) { //used to set update the state for each of the inputs, it takes the name from the input (which is the same as the state name) and updates it
+    //this.setState({value: event.target.value});
+    const target = event.target;
+    //const value = target.type === 'checkbox' ? target.checked : target.value;
+    const value = target.value;
+    const name = target.name;
+
+    //console.log(value);
+
+    this.setState({
+      [name]: value
+    });
+    }
+
+    handleSubmit(event) {
+      event.preventDefault();
+      //this.state.firstInput,this.state.lastInput,this.state.emailInput,this.state.phoneInput these values need to be sent to the spreadsheet in here
+      //Need to have a check to see that required fields are filled in (basic checking done)
+      //There will then be a redirect to the waiver page
+      console.log("signin submitted");
+      //window.location = '/waiver/' // Redirects to waiver page
+    }
+
+
   render() {
     return (
       <div className="App">
       <div className="App-header">
-          <img className="logo" src={logo}  alt="logo"  width="400px"/>
+        <h1>Maker Cube Sign-In Form</h1>
+          {/*<img className="logo" src={logo}  alt="logo"  width="400px"/>*/}
         </div>     
-                  <p>hello</p>
+                  <p>hello to everyones</p>
+
+                  <form onSubmit={this.handleSubmit} method="post" encType="multipart/form-data" > {/*Use the handleSubmit function above to submit the data(text) from the fields below*/}
+            <label>
+              <h3>Please sign in:</h3>
+                
+                    First Name:<input name="firstInput" type="text" value={this.state.firstInput} onChange={this.handleChange} className="form-control"/><br/> {/*for each of the inputs it should update its state value on every change, as this is what is submitted*/}
+                    Last Name:<input name="lastInput" type="text" value={this.state.lastInput} onChange={this.handleChange} className="form-control"/><br/>
+                    Email:<input name="emailInput" type="text" value={this.state.emailInput} onChange={this.handleChange} className="form-control"/><br/>
+                    Phone Number:<input name="phoneInput" type="text" value={this.state.phoneInput} onChange={this.handleChange} className="form-control"/><br/>
+
+
+            </label><br/>
+            {isEnabled =
+              this.state.firstInput.length > 0 
+              && this.state.lastInput.length > 0
+              && this.state.emailInput.length > 0
+              && this.state.phoneInput.length > 0}
+            <input disabled={!isEnabled} type="submit" value="Submit" className="btn btn-primary"/>
+          </form>
 
           
 
